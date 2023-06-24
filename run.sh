974afcb72c27b368b9647f2af4fb92150419ade3
Alex Taylor <alex@belphegor>
2023-06-23 22:19:50 -0400

    dont need data dir for motion

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ docker build -t motion .
 docker stop motion
 docker rm motion
 mkdir config &> /dev/null
-docker run -d --device=/dev/video0:/dev/video0 --network=docker_belphegornet --name=motion -u 1000:044 -v "$(pwd)"/config:/config:ro -v "$(pwd)"/data:/data motion
+docker run -d --device=/dev/video0:/dev/video0 --network=docker_belphegornet --name=motion -u 1000:044 -v "$(pwd)"/config:/config:ro -v motion
